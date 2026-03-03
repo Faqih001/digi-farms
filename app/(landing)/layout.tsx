352457b4d2011a13a4fac28434783f36bb0fb5f8
Faqih001 <fakiiahmad001@gmail.com>
2026-03-03 17:06:06 +0300

    refactor: remove MobileNav component from LandingLayout for cleaner code

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
-import MobileNav from "@/components/layout/mobile-nav";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/lib/auth";
 
@@ -11,7 +10,6 @@ export default async function LandingLayout({ children }: { children: React.Reac
       <Header />
       <main className="min-h-screen pb-16 md:pb-0">{children}</main>
       <Footer />
-      <MobileNav />
     </SessionProvider>
   );
 }
